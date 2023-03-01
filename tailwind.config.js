bd84978df678d5aa01d9b85ab75dbd8e3f38525d
sh2orc <sh2orc@gmail.com>
2023-03-01 22:08:54 +0900

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.js",
+    "./layouts/**/*.js",
+    "./components/**/*.js"
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
